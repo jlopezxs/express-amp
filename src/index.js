7cf2a1c6f5ded4567e25a3f731dad3d8bcf1bfdf
bluedge <nico@bluedge.co.uk>
2017-09-20 13:49:50 -0700

    Update index.js
    Check if renderOpts is defined first.

--- src/index.js
+++ src/index.js
@@ -18,13 +18,15 @@ function expressAMP ({
 
     if (override === false) {
       res.renderAMP = function (view, renderOpts, callback) {
-        renderOpts.isAMP = true
+        if (renderOpts)
+          renderOpts.isAMP = true
         this.render(view, renderOpts, renderCallback(callback))
       }
     } else {
       res.oldRenderMethod = res.render
       res.render = function (view, renderOpts, callback) {
-        renderOpts.isAMP = true
+        if (renderOpts)
+          renderOpts.isAMP = true
         this.oldRenderMethod(view, renderOpts, renderCallback(callback))
       }
     }
